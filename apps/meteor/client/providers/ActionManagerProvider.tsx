156a1e23a0e9e656a9d179aef55ea35ea645bc27
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-10-05 17:01:18 -0300

    Fix `this` reference

--- apps/meteor/client/providers/ActionManagerProvider.tsx
+++ apps/meteor/client/providers/ActionManagerProvider.tsx
@@ -16,7 +16,7 @@ const ActionManagerProvider = ({ children }: ActionManagerProviderProps): ReactE
 	useAppTranslations();
 	useAppActionButtons();
 	useAppSlashCommands();
-	useAppUiKitInteraction(actionManager.handlePayloadUserInteraction);
+	useAppUiKitInteraction(actionManager.handlePayloadUserInteraction.bind(actionManager));
 
 	return <ActionManagerContext.Provider value={actionManager}>{children}</ActionManagerContext.Provider>;
 };
